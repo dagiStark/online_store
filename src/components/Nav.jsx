4956570a3366e0ed46325b8211a9a622e9ee059a
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-07-17 23:55:21 +0300

    added button, edited nav and hero

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,13 +12,16 @@ const Nav = () => {
 
         <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
           {navLinks.map((item)=>(
-            <li key={item.href}>
+            <li key={item.label}>
               <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   )
